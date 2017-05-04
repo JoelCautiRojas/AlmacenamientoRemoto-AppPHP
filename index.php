95aaa6f4959119575186e3f05d49a15c275755ae
Joel Cauti Rojas <joel.cauti@gmail.com>
2017-05-04 09:13:59 -0500

    AlmacenamientoRemoto-AppPHP
    AlmacenamientoRemoto-AppPHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script>
+		document.location="https://www.google.com";
+	</script>
+</head>
+<body>
+</body>
+</html>
